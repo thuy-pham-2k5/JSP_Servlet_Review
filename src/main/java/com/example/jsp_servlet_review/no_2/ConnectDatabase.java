161537ce94fab3e0987027938c4493e897c06b40
Thuy Pham <phamthikimthuy05@gmail.com>
2025-01-09 08:23:09 +0700

    connect database completed

--- src/main/java/com/example/jsp_servlet_review/no_2/ConnectDatabase.java
+++ src/main/java/com/example/jsp_servlet_review/no_2/ConnectDatabase.java
@@ -1,21 +1,25 @@
-package com.example.jsp_servlet_review.no_1.database;
+package com.example.jsp_servlet_review.no_2;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDatabase {
-    public static Connection getConnection() {
-        Connection connection = null;
+    public static Connection getConnection() throws SQLException {
+        final String username = "root";
+        final String password = "1234";
+        final String urlJDBC = "jdbc:mysql://127.0.0.1:3306/jsp_servlet_review";
+        Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String username = "root";
-            String password = "1234";
-            String urlJDBC = "jdbc:mysql://localhost:3306/jsp_servlet_review";
             connection = DriverManager.getConnection(urlJDBC, username, password);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
         return connection;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        getConnection();
     }
 }
